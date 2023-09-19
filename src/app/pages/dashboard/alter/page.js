34233a8580c2bf9797e511235cb442ffed74aaaa
FilipePereiraLima <filipe.lima4@estudante.ifms.edu.br>
2023-09-19 08:01:20 -0400

    feat: add nav in alter page

--- src/app/pages/dashboard/alter/page.js
+++ src/app/pages/dashboard/alter/page.js
@@ -1,6 +1,8 @@
 'use client'
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import styles from '../../../../page.module.css'
+
 
 export default function Login() {
 
@@ -11,7 +13,30 @@ export default function Login() {
 
   }
   return (
-    <div >
+
+   <div >
+<div>
+
+<nav className={styles.nav}>
+     <div className="-mb-px flex justify-center">
+         <a className="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" href="#">
+             Home
+         </a>
+         <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="#">
+             Products
+         </a>
+         <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="#">
+             Discounts
+         </a>
+         <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3" href="#">
+             Customers
+         </a>
+     </div>
+</nav>
+
+</div>
+    
+
       <h1>Alterar</h1>
       <form onSubmit={handlerChange}>
         <input  placeholder='Name' type="text" required></input>
