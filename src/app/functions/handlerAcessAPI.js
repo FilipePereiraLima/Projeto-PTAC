95645043f65796dd54b0d812d9a55cf34b02377c
FilipePereiraLima <filipe.lima4@estudante.ifms.edu.br>
2023-10-24 17:35:40 -0400

    feat: aula 24-10

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,6 +1,6 @@
 'use server'
 
-const url = "https://aula-17-10-gamma-nine.vercel.app";
+const url = "https://aula-17-10-pi.vercel.app";
 const getUserAuthenticated = async (user) => {
     const responseOfApi = await fetch(url + "/user/authenticated",
         {
@@ -10,12 +10,22 @@ const getUserAuthenticated = async (user) => {
         }
     );
     const userAuth = await responseOfApi.json();
+    console.log(userAuth)
     return userAuth;
 }
 
 
 const getUsers = async () =>{
-        
+        try{
+            const responseOfApi = await fetch (url + "/users",{
+                next:{revalidate:10}
+            });
+            const listUsers = responseOfApi.json();
+
+            return listUsers
+        }catch{
+            return null;
+        }
 }
 
 export { getUsers, getUserAuthenticated };
